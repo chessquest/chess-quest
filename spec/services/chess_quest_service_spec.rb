af50fa95e00665b33d5448a37793bbba1a194c37
Ely Hess <Ely.hess@me.com>
2021-02-23 19:04:19 -0700

    Add chessquestservice spec
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

---
+++ spec/services/chess_quest_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe ChessQuestService do
+	describe "class methods" do
+		describe "::get_fen" do
+			it 'gets a game ' do
+				json_response = File.read("spec/fixtures/game.json")
+				stub_request(:get, "https://https://chess-com-api.herokuapp.com/api/v1/game?find=magnus").
+					to_return(status: 200, body: json_response)
+
+				game = ChessQuestService.get_fen("magnus")
+
+				expect(game).to be_a Hash
+				expect(game).to have_key(:data)
+				expect(game[:data]).to be_a Hash
+				expect(game[:data]).to have_key(:attributes)
+				expect(game[:data][:attributes][:fen]).to eq("this_is_the_fen_string")
+			end
+		end
+	end
+end
