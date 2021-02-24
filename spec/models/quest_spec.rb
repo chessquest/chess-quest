064dfc0e40b51b1f09d2a517c7d6fa9a3a3149c0
Ely Hess <Ely.hess@me.com>
2021-02-23 17:11:21 -0700

    Add rspec config
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

---
+++ spec/models/quest_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe , type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :}
+    it {should have_many :}
+  end
+
+  describe 'instance methods' do
+  end
+
+  describe 'class methods' do 
+  end
+  
+end
+
