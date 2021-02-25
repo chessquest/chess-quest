c238ee4edb454e07c25648cd8ad23d92f8fbd4db
Grayson Myers <graymyers10@gmail.com>
2021-02-25 13:53:32 -0700

    Update tests to reflect new columns
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 	describe 'validations' do
 		it { should validate_presence_of :status }
-		it { should validate_presence_of :fen }
+		it { should validate_presence_of(:current_fen).on(:update) }
+		it { should validate_presence_of :starting_fen }
 	end
 
 	describe 'relationships' do
