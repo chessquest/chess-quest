959faf8d149d635b0eeedf4d8e05892b3ee944a8
Adam Etzion <adametzion90@gmail.com>
2021-02-24 19:00:12 -0700

    Add quest controller to create quest and accompanying serializer and requst spec
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

---
+++ spec/requests/api/v1/quests_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Games API' do
+  describe 'games create' do  
+    describe 'happy path' do
+      it 'creates a quest' do
+        quest_params = {user_id: 2}
+				headers = {'CONTENT_TYPE' => 'application/json'}
+				post '/api/v1/quests', headers: headers, params: JSON.generate(quest_params)
+
+        created_quest = Quest.last
+        expect(response).to be_successful
+        
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(201)
+        expect(parsed_response).to be_a Hash
+        expect(parsed_response[:data][:attributes][:status]).to eq(created_quest.status)
+        expect(parsed_response[:data][:attributes][:status]).to eq('in_progress')
+        expect(parsed_response[:data][:attributes][:user_id]).to eq(created_quest.user_id)
+
+      end
+
+      it "can convert quest to JSON and send upon creation" do
+      end
+    end
+
+    describe 'sad path' do
+      it "i dunno" do
+        
+      end
+    end
+  end
+end
