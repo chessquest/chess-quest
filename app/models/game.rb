9d2b413297a3fe4f60d0a1e27029edd58891a606
Grayson Myers <graymyers10@gmail.com>
2021-02-25 13:57:21 -0700

    Update validations to include new columns
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,8 @@ class Game < ApplicationRecord
   belongs_to :quest
 
   validates_presence_of :status
-  validates_presence_of :fen
+  validates_presence_of :starting_fen
+  validates_presence_of :current_fen, on: :update
 
   enum status: [:in_progress, :won, :lost]
 end
