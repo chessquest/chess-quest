fc646c3a5de810a7a1149c61b2385c8bc69d6458
Max Ribbans <ribbans.max@gmail.com>
2021-02-27 12:14:38 -0500

    Move quests and games path under namespace users
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
 	  namespace :v1 do
 
-		  resources :quests, except: [:destroy]
-
-		  resources :games, except: [:destroy]
-
+			
+			
 		  namespace :users do
+				resources :quests, except: [:destroy]
+				resources :games, except: [:destroy]
 			  get '/:id/win_streak', to: 'stats#win_streak'
 			  get '/:id/win_loss', to: 'stats#win_loss'
 			  get '/:id/pieces', to: 'stats#pieces'
