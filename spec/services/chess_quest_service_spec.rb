bb65ff430d4699515d6c52c31f51fc9350beb99c
Phil McCarthy <philmcc2631@gmail.com>
2021-02-27 17:38:31 -0700

    Add VCR to tests and add cassettes
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- spec/services/chess_quest_service_spec.rb
+++ spec/services/chess_quest_service_spec.rb
@@ -3,18 +3,13 @@ require 'rails_helper'
 describe GameService do
 	describe "class methods" do
 		describe "::get_fen" do
-			it 'gets a game ' do
-				json_response = File.read("spec/fixtures/game.json")
-				stub_request(:get, "https://chess-com-api.herokuapp.com/api/v1/game?find_player=magnus").
-					to_return(status: 200, body: json_response)
-
-				game = GameService.get_game_data("magnus")
+			it 'gets a game', :vcr do
+				game = GameService.get_game_data("magnuscarlsen")
 
 				expect(game).to be_a Hash
 				expect(game).to have_key(:data)
 				expect(game[:data]).to be_a Hash
 				expect(game[:data]).to have_key(:attributes)
-				expect(game[:data][:attributes][:fen]).to eq("this_is_the_fen_string")
 			end
 		end
 	end
