384df1ec3564bb1d862bad1f7a0b517413c4f52f
Grayson Myers <graymyers10@gmail.com>
2021-03-01 00:56:25 -0700

    Add games update request

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -22,4 +22,16 @@ class Api::V1::GamesController < ApplicationController
 		games = quest.games
 		render json: GameSerializer.new(games)
 	end
+
+	def update
+		# could define game_params here as a refactor, but only 2 things will realistically be changing
+		game = Game.find(params[:id])
+		game.current_fen = params[:current_fen]
+		game.status = params[:status]
+		if game.save
+			render json: GameSerializer.new(game)
+		else
+			# sad path goes here
+		end
+	end
 end
