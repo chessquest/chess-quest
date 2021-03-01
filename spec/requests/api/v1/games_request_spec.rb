994a0c76bcce2d965d1ae652a24886a652594f08
Grayson Myers <graymyers10@gmail.com>
2021-03-01 00:32:24 -0700

    Add test for games index request

--- spec/requests/api/v1/games_request_spec.rb
+++ spec/requests/api/v1/games_request_spec.rb
@@ -1,6 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Games API' do
+  describe "games index", :vcr do
+    describe "happy path" do
+      it "returns a user's games that are associated with given quest" do
+        user_id = 1
+        num_games = 4
+        current_quest = Quest.create(user_id: user_id)
+        some_other_quest = Quest.create(user_id: user_id)
+
+        games = (0..num_games-1).map do |i|
+          Game.create(quest: current_quest, status: [:in_progress, :won, :lost].sample, starting_fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        end
+
+        # creates extra games and verifies through the count that these are not included in the index for the other quest.
+        other_quest_games = (0..5).map do |i|
+          Game.create(quest: some_other_quest, status: [:in_progress, :won, :lost].sample, starting_fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        end
+
+        get "/api/v1/users/#{user_id}/quests/#{current_quest.id}/games"
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(parsed_response[:data].count).to eq(num_games)
+
+        num_games.times do |i|
+          game = games[i]
+          expect(parsed_response[:data][i][:id].to_i).to eq(game.id)
+          expect(parsed_response[:data][i][:attributes][:status]).to eq(game.status)
+          expect(parsed_response[:data][i][:attributes][:starting_fen]).to eq(game.starting_fen)
+        end
+      end
+    end
+
+    describe "sad path" do
+
+    end
+  end
+
   describe 'games create' do
     describe 'happy path' do
       it 'creates a game', :vcr do
