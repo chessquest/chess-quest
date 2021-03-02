69b5626694c9d815ad2cb128b14b26fa5d9885bd
Adam Etzion <adametzion90@gmail.com>
2021-03-01 18:31:56 -0700

    Refactor games controller create and index methods into games facade

--- app/facades/game_facade.rb
+++ app/facades/game_facade.rb
@@ -1,7 +1,20 @@
 class GameFacade
 	class << self
 		# MAKE BROADER THAN JUST FEN IF WE NEED MORE DATA DOWN THE LINE
-		def get_fen(params)
+		def quest_games(params)
+			quest = Quest.find(params[:quest_id])
+			quest.games	
+		end
+		
+		def create_game(params)
+			quest = Quest.where('user_id = ? AND status = ?', params[:user_id], 0).first
+			fen = get_fen(params[:find_player])
+			fen_poro = Fen.new(fen)
+			fen_poro.to_starting_position
+			Game.create!(starting_fen: fen_poro.fen, quest: quest)	
+		end
+		
+		def get_fen(params)	
 			chess_game = GameService.get_game_data(params)
 			chess_game[:data][:attributes][:fen]
 		end	
