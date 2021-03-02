b61c5445984fa9ad6e49f0515f54910e53deb960
Grayson Myers <graymyers10@gmail.com>
2021-03-01 18:53:17 -0700

    Add top quests controller action and quest stat serializer

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::StatsController < ApplicationController
+  NUM_TOP_QUESTS = 3
+
   def game_streak
     user_id = params[:user_id]
     quest = Quest.where('user_id = ? AND status = ?', user_id, 0).first
     render json: UserStatSerializer.new(quest)
+  end
+
+  def top_quests
+    quests = Quest.joins(:games)
+      .select("quests.*, count(games.status) as num_wins")
+      .where("games.status = '1'")
+      .group('quests.id')
+      .order("num_wins DESC")
+      .limit(NUM_TOP_QUESTS)
+
+    render json: QuestStatSerializer.new(quests)
   end
 end
