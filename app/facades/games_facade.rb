b4cd38ea2b9edb27d8a7104856537da876b3fda7
Liam Cusack <LCusack8108@gmail.com>
2021-03-01 21:18:53 -0700

    Add default board to classic game, adjust create_game method in facade, add testing.
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- app/facades/games_facade.rb
+++ app/facades/games_facade.rb
@@ -3,31 +3,35 @@ class GamesFacade
 		# MAKE BROADER THAN JUST FEN IF WE NEED MORE DATA DOWN THE LINE
 		def quest_games(params)
 			quest = Quest.find(params[:quest_id])
-			quest.games	
+			quest.games
 		end
 
 		def find_game(params)
 			Game.find(params.to_i)
 		end
-		
+
 		def create_game(params)
 			quest = Quest.where('user_id = ? AND status = ?', params[:user_id], 0).first
-			fen = get_fen(params[:find_player])
+			if params[:find_player] == "playclassic"
+				fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+			else
+				fen = get_fen(params[:find_player])
+			end
 			fen_poro = Fen.new(fen)
 			fen_poro.to_starting_position
-			Game.create!(starting_fen: fen_poro.fen, quest: quest)	
+			Game.create!(starting_fen: fen_poro.fen, quest: quest)
 		end
 
 		def update_game(params)
 			game = Game.find(params[:id])
 			game.current_fen = params[:current_fen]
-			game.status = params[:status]	
+			game.status = params[:status]
 			game
 		end
-		
-		def get_fen(params)	
+
+		def get_fen(params)
 			chess_game = GameService.get_game_data(params)
 			chess_game[:data][:attributes][:fen]
-		end	
+		end
 	end
 end
