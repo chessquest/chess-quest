3cc40d2aa3c8f4b0ca1c3f57f76d9a4e320e432f
Grayson Myers <graymyers10@gmail.com>
2021-03-01 17:57:20 -0700

    Refactor hash formatting into serializer

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -2,17 +2,6 @@ class Api::V1::StatsController < ApplicationController
   def game_streak
     user_id = params[:user_id]
     quest = Quest.where('user_id = ? AND status = ?', user_id, 0).first
-
-    temp = {
-      data: {
-        type: "user_stats",
-        attributes: {
-          quest_id: quest.id,
-          streak: quest.games.where(status: :won).count
-        }
-      }
-    }
-
-    render json: temp
+    render json: UserStatSerializer.new(quest)
   end
 end
